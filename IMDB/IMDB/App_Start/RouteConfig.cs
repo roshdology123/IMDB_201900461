6adc8e215a769b21296b4e4207998fcd375e098e
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2022-04-19 01:21:15 +0200

    Default HomePage

--- IMDB/IMDB/App_Start/RouteConfig.cs
+++ IMDB/IMDB/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace IMDB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HomePage", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }
