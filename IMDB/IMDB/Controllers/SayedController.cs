f75cdcd6c76c314e8a4e48204ceff41b11a6d639
momran0123s <102372842+momran0123s@users.noreply.github.com>
2022-04-19 05:45:55 +0200

    admin-bar

---
+++ IMDB/IMDB/Controllers/SayedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMDB.Controllers
+{
+    public class SayedController : Controller
+    {
+        // GET: Sayed
+        public ActionResult Sayed()
+        {
+            return View();
+        }
+    }
+}
