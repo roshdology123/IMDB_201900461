7f8e958b14e4b5a7920ffe19513086136a42aa40
momran0123s <102372842+momran0123s@users.noreply.github.com>
2022-04-21 07:53:07 +0200

    movies

---
+++ IMDB/IMDB/Content/Sayed.js
@@ -0,0 +1,33 @@
+﻿function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function details() {
+    var x = document.getElementById("movie-details");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function details2() {
+    var x = document.getElementById("movie-details2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
