4138581ef1b8da181c84ad134f19a856539ab045
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2022-04-25 16:49:07 +0200

    Image Conversion from and to byte array

---
+++ IMDB/IMDB/Controllers/ImageConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace IMDB.Controllers
+{
+    public class ImageConversion
+    {
+        public byte[] ImageToByteArray(Image imageIn)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                imageIn.Save(memoryStream, imageIn.RawFormat);
+                return memoryStream.ToArray();
+            }
+        }
+        public Image ByteArrayToImage(byte[] source)
+        {
+            MemoryStream memoryStream = new MemoryStream(source);
+            Image image = Image.FromStream(memoryStream);
+            return image;
+        }
+    }
+}
