9dbc67e9b090647ca181ff577fe9d06a5b7eb9c4
Abdallah Assem <102911851+Abdallah-Assem@users.noreply.github.com>
2022-04-25 18:11:43 +0200

    User Settings Page

---
+++ IMDB/IMDB/Content/UploadImg.js
@@ -0,0 +1,13 @@
+﻿var loadFile = function (event) {
+    var image = document.getElementById("output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function ShowPassword() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+} 
