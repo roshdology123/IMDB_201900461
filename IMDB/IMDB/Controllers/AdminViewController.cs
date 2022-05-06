a31ece46b9b3243796a1d41a49d00916c0c00b67
momran0123s <102372842+momran0123s@users.noreply.github.com>
2022-05-06 23:48:41 +0200

    new-design

--- IMDB/IMDB/Controllers/AdminViewController.cs
+++ IMDB/IMDB/Controllers/AdminViewController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IMDB.DataLayer;
+using IMDB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,7 @@ namespace IMDB.Controllers
 {
     public class AdminViewController : Controller
     {
+        private IMdbDBContext db = new IMdbDBContext();
         // GET: AdminHomePage
         public ActionResult AdminHomePage()
         {
@@ -21,20 +24,54 @@ namespace IMDB.Controllers
         {
             return View();
         }
+        
+        // GET: Actors
         public ActionResult Actors()
         {
-            return View();
+            List<Actor> Actors = db.Actors.ToList();
+            return View(Actors);
         }
+
+        [HttpGet]
         public ActionResult AddActors()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult AddActors(Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Actors.Add(actor);
+                db.SaveChanges();
+                return RedirectToAction("Actors");
+            }
+            return View();
+        }
+
         public ActionResult Directors()
         {
-            return View();
+            List<Director> Directors = db.Directors.ToList();
+            return View(Directors);
         }
         public ActionResult AddDirectors()
         {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AdddDirectors()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddDirectors(Director director)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Directors.Add(director);
+                db.SaveChanges();
+                return RedirectToAction("Directors");
+            }
             return View();
         }
     }
