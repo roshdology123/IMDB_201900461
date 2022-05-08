428f78509793b8145d210ac4b5ddf77be0290876
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2022-05-08 14:24:45 +0200

    Created Search View

---
+++ IMDB/IMDB/ViewModel/SearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IMDB.Models;
+
+namespace IMDB.ViewModel
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Actor> Actors { get; set; }
+        public IEnumerable<Movie> Movies { get; set; }
+        public IEnumerable<Director> Directors { get; set; }
+
+    }
+}
